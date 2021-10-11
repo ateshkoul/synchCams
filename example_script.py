d005580d745b91001971364c9a719867104e14a3
Atesh <atesh.koul@gmail.com>
2021-10-11 11:22:10 +0200

    ready for pypi

--- example_script.py
+++ example_script.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  8 17:31:36 2021
 
-@author: Atesh
-"""
-
-import sys
-sys.path.insert(0,"E:\\Libraries\\")
 from synchCams.synchCams import synchCams
-exp = synchCams([0,1])
-exp.start_experiment()
-exp.releaseCams()
+if __name__ == "__main__":
+    exp = synchCams([0,1])
+    exp.start_experiment()
+    exp.releaseCams()
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 setup(
-    name='synchcams',
+    name='synchCams',
     version='0.1',
     license='MIT',
     author="Atesh Koul",
     author_email='atesh.koul@gmail.com',
-    packages=['synchcams'],
+    packages=['synchCams'],
     url = 'https://github.com/ateshkoul/SynchCams',   # Provide either the link to your github or to your website  
     download_url = 'https://github.com/ateshkoul/SynchCams/archive/v_01.tar.gz',    # I explain this later on
     keywords=['synch cams','dual','cams'],
